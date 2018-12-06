b01a305f8cd0cbd05c67e43648ec47a37f8bb7c7
liuyihuicaicloud <liuyihui@caicloud.io>
2018-12-06 14:15:20 +0800

    model serving

--- pytorch_support/graphpipe_client.py
+++ pytorch_support/graphpipe_client.py
@@ -1,16 +1,3 @@
-from io import BytesIO
-from PIL import Image, ImageOps
-import numpy as np
-import requests
-
-from graphpipe import remote
-
-data = np.array(Image.open("/Users/caicloud 1/Documents/kubeflow-admin/docs/pytorch_support/mnist-pytorch/images/1.jpg"))
-data = data.reshape([1] + list(data.shape))
-data = np.rollaxis(data, 1, 1).astype(np.float32)  # channels first
-data = np.expand_dims(data, axis=0)
-data.astype(np.float32)
-print(data.shape)
-
-pred = remote.execute("http://192.168.22.197:30894", data)
-print(pred)
+version https://git-lfs.github.com/spec/v1
+oid sha256:b27912cbe86ecce4b7254af9c6338bccf50a9043aeda9f70eceb0d2064e338ef
+size 505
